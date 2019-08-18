008cfc9b9a818cb0bdcfae54419a80d1cf753938
Gustav Carlstedt <gct14001@student.mdh.se>
2019-08-18 17:42:10 +0200

    Add project files.

---
+++ PlayCardApp/PlayCardApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PlayCardApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void NewGamePressed(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "First button pressed";
+        }
+
+        private void JoinGamePressed(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Second button pressed";
+        }
+    }
+}
